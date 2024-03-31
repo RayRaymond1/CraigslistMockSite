99bd4bc74f1389c4eb7e0109e3db74e249d88fed
RayRaymond1 <raymond.fogle0@gmail.com>
2024-03-31 18:04:37 -0400

    Front Page Progress (i hate sizing divs)

--- craigsmock/src/app/page.tsx
+++ craigsmock/src/app/page.tsx
@@ -1,15 +1,47 @@
 import Image from "next/image";
+import './globals.css'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex items-center place-content-center  p-24">
       <LeftBar/>
+      <TopBar/>
+      <Categories/>
     </main>
   );
 }
 
 export function LeftBar(){
   return(
-    <div className=""> hello!</div>
+    <div className="flex flex-col items-center bg-slate-200 border-x-2 border-slate-500 p-5 "> 
+      <div className="flex items-center place-content-center mb-5"><Image src="/testemblem.png" height={50} width={50} alt=""/> <a href="/">Ray's List</a></div>
+      <div className="flex items-center place-content-center bg-white mb-10 p-2"><Image src="/compose.svg" height={30} width={30} alt=""/><h2 className="text-center text-teal-500"><a href="/createPost">Post an ad</a></h2></div>
+      
+      <div className="mb-1"><a href="/faq">help, faq, abuse, legal</a></div>
+      <div className="mb-1"><a href="/safety">avoid scams & fraud</a></div>
+      <div className="mb-10"><a href="/tips">personal safety tips</a></div>
+
+      <div className="mb-1"><a href="/about">about ray's list</a></div>
+      <div className="mb-1"><a href="/patchNotes">what's new</a></div>
+      <div className="mb-1"><a href="/status">system status</a></div>
+    </div>
+  );
+}
+
+export function Categories(){
+  return (
+    <div>
+      Hello!
+    </div>
+  );
+}
+
+export function TopBar(){
+  return(
+    <div className="flex justify-between bg-slate-200 border-x-2 border-slate-500 mx-1 ">
+      <p className="text-4xl">Philadelphia</p>
+      <div><div className=""> <a href="/"><Image src="/compose.svg" height={50} width={50} alt=""/>Ray's List</a></div></div>
+    </div>
   );
 }
